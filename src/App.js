1e437b7286837f5d50f4a74411c91b77ef86d1a0
KaioLN <Kaio.LeonardoSS@gmail.com>
2023-12-15 14:59:59 -0300

    praticando-react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>TeclaT new Developer</h1>
+      <h1> TeclaT new Developer </h1>
       <h2> Junior FrontEnd Developer </h2>
     </div>
   );
